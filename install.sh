1b74c1d633fe18775f03892cdc9e6c62b4f32b7b
Chantel Diaz <99683409+chantel-marie-diaz@users.noreply.github.com>
2025-09-25 07:56:03 -0600

    Updating install.sh

--- install.sh
+++ install.sh
@@ -10,21 +10,21 @@ chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install terminator
-sudo apt-get install terminator
+sudo apt-get install terminator -y
 
 # Install python3.9
 sudo apt update
-sudo apt install software-properties-common
+sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt install python3.9
+sudo apt install python3.9 -y
 
 # Install python virtual environments
-sudo apt install python3.10-venv
-sudo apt install python3.9-venv
+sudo apt install python3.10-venv -y
+sudo apt install python3.9-venv -y
 
 # Install git
 sudo apt-get update
-sudo apt-get install git
+sudo apt-get install git -y
 git config --global user.name "Chantel Diaz"
 git config --global user.email "cdiaz@consumer-edge.com"
 
@@ -38,7 +38,7 @@ sudo apt-get remove docker docker-engine docker.io containerd runc
 ## Install apt repo for docker
 sudo apt-get update
 sudo apt-get install ca-certificates curl gnupg
-sudo install -m 0755 -d /etc/apt/keyrings
+sudo install -m 0755 -d /etc/apt/keyrings -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 echo \
@@ -48,20 +48,20 @@ echo \
 
 ## Install docker engine
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 ### sudo docker run hello-world
 
 # Install rclone
-curl https://rclone.org/install.sh | sudo bash
+curl https://rclone.org/install.sh | sudo bash -y
 
 # Install py dev env
-sudo apt-get install python-dev-is-python3
+sudo apt-get install python-dev-is-python3 -y
 
 # Install filezilla
-sudo apt-get install filezilla
+sudo apt-get install filezilla -y
 
 # Install traceroute
-sudo apt-get install traceroute
+sudo apt-get install traceroute -y
 
 # Install figlet
 sudo apt-get install figlet
@@ -94,7 +94,7 @@ sudo apt-get install unzip -y
 sudo apt-get install htop -y
 
 # Install tmux
-sudo apt-get install tmux
+sudo apt-get install tmux -y
 
 # Install postgres and tooling
 ## Do not forget, `sudo systemctl disable postgresql`
@@ -103,7 +103,7 @@ sudo apt-get install -y \
   pgcli
 
 # Install tasking tool
-sudo apt-get install taskwarrior
+sudo apt-get install taskwarrior -y
 
 # Install java
 sudo apt-get install default-jre -y
@@ -156,7 +156,7 @@ sudo unzip $HOME/Downloads/Hasklig.zip -d /usr/local/share/fonts
 #
 
 # Install neovim and dependencies
-sudo apt-get install neovim
+sudo apt-get install neovim -y
 sudo apt-get install -y \
   nodejs \
   npm
@@ -179,11 +179,10 @@ gcloud init # more of a placeholder
 # pick project number for stage-boat and region 1
 
 # Install kubectl
-sudo apt-get install kubectl
-
+sudo apt-get install kubectl -y
 # Install minikube
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo install minikube-linux-amd64 /usr/local/bin/minikube
+sudo install minikube-linux-amd64 /usr/local/bin/minikube -y
 
 # Install pulumi
 curl -fsSL https://get.pulumi.com | sh
@@ -191,7 +190,7 @@ curl -fsSL https://get.pulumi.com | sh
 # Install kubeseal
 wget https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.22.0/kubeseal-0.22.0-linux-amd64.tar.gz
 tar xfz kubeseal-0.22.0-linux-amd64.tar.gz
-sudo install -m 755 kubeseal /usr/local/bin/kubeseal
+sudo install -m 755 kubeseal /usr/local/bin/kubeseal -y
 
 # Install helm
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
@@ -199,11 +198,45 @@ chmod 700 get_helm.sh
 ./get_helm.sh
 
 # Install sops
-sudo dpkg -i sops_3.7.3_amd64.deb
+curl -LO https://github.com/getsops/sops/releases/download/v3.10.2/sops-v3.10.2.linux.amd64
+sudo dpkg -i sops_3.10.2_amd64.deb
 sudo apt-get install -f
 
 # Install clickhouse
 curl https://clickhouse.com/ | sh
+
+# Install to verify HashiCorp's GPG signature
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common 
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+# Install terraform
+sudo apt update
+sudo apt-get install terraform -y
+
+# Install pipx
+sudo apt update
+sudo apt install pipx
+pipx ensurepath
+
+# Install poetry
+pipx install poetry
+
+# Install snap packages
+sudo apt update
+sudo apt install snapd -y
+
+# Install slack
+sudo snap install slack
+
+# Install zoom, needs package downloaded first
+sudo apt update
+sudo apt install ~/Downloads/zoom_amd64.deb -y
+
+# Install warp terminal, probably can use curl instead of manual download
+sudo apt install ~/Downloads/warp-terminal_0.2025.09.17.08.11.stable.02_amd64.deb -y
 
 # Changing terminal to my name
 sudo hostnamectl hostname chantel
