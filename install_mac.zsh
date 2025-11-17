771f685abe2a7bf8e4f5d8e38b5c5d77a204f64e
Chantel Diaz <99683409+chantel-marie-diaz@users.noreply.github.com>
2025-11-16 19:03:20 -0700

    Create install_mac.zsh

---
+++ install_mac.zsh
@@ -0,0 +1,7 @@
+# MAKE SURE INSTALL XCODE xcode-select --install
+# download brew
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install zsh
+brew install zsh
